365b96053da08ec5c92612db5d94f8f5431645db
juansarsotti <68615256+juansarsotti@users.noreply.github.com>
2020-07-22 17:11:18 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,225 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title class="titletyped">DEMO!</title>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- GOOGLE FONTS -->
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="dist/vendor/bootstrap/css/bootstrap.min.css">
+
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="dist/vendor/animate/animate.css">
+
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="dist/css/main.min.css">
+</head>
+
+<body>
+
+<body>
+    <!-- NavBar -->
+    <div class="navbar-brand">
+        <div class="brand-logo">
+            <img src="#" alt="logo">
+        </div>
+        <nav>
+            <ul class="nav-list"> 
+                <li class="nav-item"><a href="#">Consecionarios</a></li>
+                <li class="nav-item"><a href="#">Nuestros Productos</a></li>
+                <li class="nav-item"><a href="#"><img src="#" alt="mail-icon">Contacto</a></li>
+                <li class="nav-item"><a href="#"><img src="#" alt="tel-icon"><span>TEL</span> 4552-8015</a></li>
+                <li class="nav-item btn"><a href="#">Whatsapp <img src="#" alt="wsp-icon"></a></li>
+            </ul>
+        </nav>
+    </div>
+    <!-- Hero -->
+    <section class="hero">
+        <div class="hero-box">
+            <h1 class="hero-text"><span>Movilidad</span> Electrica</h1>
+        </div>
+        <div class="hero-img">
+            <img src="#" alt="electric-vehicle">
+            <p>Para que moverse sea <span>un placer</span></p>
+        </div>
+    </section>
+    <!-- Introduction-Description & Contact-Form -->
+    <section class="intro-form">
+        <div class="logo-vision">
+            <img src="#" alt="logo">
+            <p>Con la confianza de una empresa con trayectoria, pero con una visión revolucionaria del futuro.</p>
+        </div>
+        <div class="form-text">
+            <p>Si querés formar parte de nuestra red de concesionarios <span>comunicate con nosotros</span> a través de este formulario:</p>
+        </div>    
+        <form action="">
+                <div class="col-1">
+                    <label for="">
+                        <input type="text" name="name" id="name" placeholder="Nombre completo">
+                    </label>
+                    <label for="">
+                        <input type="tel" name="tel-num" id="tel-num" placeholder="Teléfono">
+                    </label>
+                    <label for="">
+                        <input type="email" name="email" id="email" placeholder="Email">
+                    </label>
+                </div>
+                <div class="col-2">
+                    <label for="">
+                        <input type="text" name="message" id="message">
+                    </label>
+                    <button type="submit" class="active"><p>Enviar</p><img src="#" alt="arrow"></button>
+                </div>
+        </form>
+    </section>
+    <!-- About us -->
+    <section class="about">
+                <p>Somos GREENMOVE SRL una empresa de movilidad sustentable, que pertenece al grupo ARTELUM con una experiencia de más de 25 años en el rubro eléctrico, importando y comercializando productos de tecnología e innovación. En esta oportunidad estamos presentando una nueva generación de productos orientados a la movilidad eléctrica y transporte urbano. </p>
+                <p>EMUV, una marca que reinventa el futuro de la movilidad eléctrica en Argentina con la confianza de una empresa con trayectoria, pero con una visión revolucionaria del futuro.</p>
+    </section>
+    <!-- Top Products navigation -->
+    <section class="top-products">
+                <h2>Productos destacados</h1>
+                <div class="card-lists-nav">
+                    <div class="card-item">
+                        <img src="#" alt="item" class="item-thumbnail">
+                        <div class="item-description">
+                            <h3 class="item-name">Grace</h3>
+                            <ul class="item-specs">
+                               <li><img src="#" alt="topspeed" class="spec-icon">Máx. <span>45km/h</span></li>
+                               <li><img src="#" alt="bat-duration" class="bat-icon">Carga en <span>6 horas</span></li>
+                               <li><img src="#" alt="capacity" class="hearth-icon">Autonomía<span>60km</span></li> 
+                            </ul>
+                        </div>
+                        <div class="checkout">
+                            <h3 class="price">80000</h3> <!-- TODO BEFORE AR$ (CSS) -->
+                            <button class="active">Comprar <img src="#" alt="arrow"></button>
+                        </div>
+                    </div>
+                    <div class="card-item">
+                        <img src="#" alt="item" class="item-thumbnail">
+                        <div class="item-description">
+                            <h3 class="item-name">Grace</h3>
+                            <ul class="item-specs">
+                               <li><img src="#" alt="topspeed" class="spec-icon">Máx. <span>45km/h</span></li>
+                               <li><img src="#" alt="bat-duration" class="bat-icon">Carga en <span>6 horas</span></li>
+                               <li><img src="#" alt="capacity" class="hearth-icon">Autonomía<span>60km</span></li> 
+                            </ul>
+                        </div>
+                        <div class="checkout">
+                            <h3 class="price">80000</h3> <!-- TODO BEFORE AR$ (CSS) -->
+                            <button class="active">Comprar <img src="#" alt="arrow"></button>
+                        </div>
+                    </div>
+                    <div class="card-item">
+                        <img src="#" alt="item" class="item-thumbnail">
+                        <div class="item-description">
+                            <h3 class="item-name">Grace</h3>
+                            <ul class="item-specs">
+                               <li><img src="#" alt="topspeed" class="spec-icon">Máx. <span>45km/h</span></li>
+                               <li><img src="#" alt="bat-duration" class="bat-icon">Carga en <span>6 horas</span></li>
+                               <li><img src="#" alt="capacity" class="hearth-icon">Autonomía<span>60km</span></li> 
+                            </ul>
+                        </div>
+                        <div class="checkout">
+                            <h3 class="price">80000</h3> <!-- TODO BEFORE AR$ (CSS) -->
+                            <button class="active">Comprar <img src="#" alt="arrow"></button>
+                        </div>
+                    </div>
+                    <div class="card-item">
+                        <img src="#" alt="item" class="item-thumbnail">
+                        <div class="item-description">
+                            <h3 class="item-name">Grace</h3>
+                            <ul class="item-specs">
+                               <li><img src="#" alt="topspeed" class="spec-icon">Máx. <span>45km/h</span></li>
+                               <li><img src="#" alt="bat-duration" class="bat-icon">Carga en <span>6 horas</span></li>
+                               <li><img src="#" alt="capacity" class="hearth-icon">Autonomía<span>60km</span></li> 
+                            </ul>
+                        </div>
+                        <div class="checkout">
+                            <h3 class="price">80000</h3> <!-- TODO BEFORE AR$ (CSS) -->
+                            <button class="active">Comprar <img src="#" alt="arrow"></button>
+                        </div>
+                    </div>
+                    <div class="card-item">
+                        <img src="#" alt="item" class="item-thumbnail">
+                        <div class="item-description">
+                            <h3 class="item-name">Grace</h3>
+                            <ul class="item-specs">
+                               <li><img src="#" alt="topspeed" class="spec-icon">Máx. <span>45km/h</span></li>
+                               <li><img src="#" alt="bat-duration" class="bat-icon">Carga en <span>6 horas</span></li>
+                               <li><img src="#" alt="capacity" class="hearth-icon">Autonomía<span>60km</span></li> 
+                            </ul>
+                        </div>
+                        <div class="checkout">
+                            <h3 class="price">80000</h3> <!-- TODO BEFORE AR$ (CSS) -->
+                            <button class="active">Comprar <img src="#" alt="arrow"></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="more-info">
+                    <div class="tech-info">
+                        <p>Para más información de los disponibles:</p>
+                        <button class="download-info active">Descargar ficha técnica <img src="#" alt="pin-icon"></button>
+                    </div>
+                    <a href="#" class="view-all-items">Ver Todos <img src="#" alt="dash"></a>
+                </div>
+    </section>
+    <!-- Carousel Gallery -->
+    <section class="gallery">
+        <img src="#" alt="gal-item" class="gallery-item">
+        <img src="#" alt="gal-item" class="gallery-item">
+        <img src="#" alt="gal-item" class="gallery-item">
+        <img src="#" alt="gal-item" class="gallery-item">
+        <img src="#" alt="gal-item" class="gallery-item">
+    </section>
+    <!-- Footer -->
+    <footer>
+        <div class="brand">
+            <img src="#" alt="logo">
+            <p>Una marca de Greenmove S.R.L.</p>
+        </div>
+            <div class="footer-nav">
+                <ul class="foot-nav-list">
+                    <a href="#"><li>Concesionarios</li></a>
+                    <a href="#"><li>Nuestros Productos</li></a>
+                    <a href="#"><li>Contacto</li></a>
+                </ul>
+            </div>
+            <div class="mercado-libre">
+                <p>Conocé nuestra tienda oficial en <span>Mercado Libre</span><img src="#" alt="ml-logo"></p>
+            </div>
+            <div class="contact-btns">
+                <div class="social-media">
+                    <a href="#" class="social-icon"><img src="#" alt="Facebook"></a>
+                    <a href="#" class="social-icon"><img src="#" alt="Instagram"></a>
+                </div>
+                <div class="contact-info">
+                    <ul class="contact-list">
+                        <a href="#"><li class="contact-item"><img src="#" alt="mail-icon">Contacto</li></a>
+                        <a href="#"><li class="contact-item"><img src="#" alt="tel-icon"><span>TEL</span> 4552-8015</li></a>
+                        <a href="#"><li class="contact-item btn">Whatsapp <img src="#" alt="wsp-icon"></li></a>
+                        <a href="#"><li class="contact-item btn">Whatsapp <img src="#" alt="wsp-icon"></li></a>
+                    </ul>
+                </div>
+            </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="dist/vendor/jquery/jquery.js"></script>
+    <script src="dist/vendor/jquery/jquery-migrate.js"></script>
+
+    <script src="dist/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="dist/js/main.min.js"></script>
+
+</body>
+
+</html>
